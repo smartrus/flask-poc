1baa905477d52b372b1e0633d4b3001d96308de1
Rustem Abdrakhmanov <rustemabd@gmail.com>
2019-09-12 09:02:59 +0400

    Templates added

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ import re
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello_there(name = None):
